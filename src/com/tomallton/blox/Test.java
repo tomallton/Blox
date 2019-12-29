0dc52c8b788a3d3c5f7808a9a82e7dac1fc4f92b
Tom Allton <tomallton1@gmail.com>
2019-12-29 22:46:22 +0000

    Add separate JSON parser.

---
+++ src/com/tomallton/blox/Test.java
@@ -0,0 +1,11 @@
+package com.tomallton.blox;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(new JsonParser("[1,2]").parse());
+
+    }
+
+}
