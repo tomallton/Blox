617a42a70c64e390e46b67c7d4fa3c8bda1ec651
Tom Allton <tomallton1@gmail.com>
2020-01-10 10:18:31 +0000

    Move to local.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -9,7 +9,7 @@ public class Test {
     public static void main(String[] args) throws Exception {
         Loader<ConsoleClient> loader = new Loader<>();
         loader.addBlockType("test.blocks");
-        loader.load(new File("/Users/Tom/Downloads/"));
+        loader.load(new File("/Users/Tom/Documents/GitHub/Blox/src/main/resources"));
     }
 
 }
